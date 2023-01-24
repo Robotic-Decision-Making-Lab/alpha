5cbb503a0d91ce1197f315463b9502f9003d9ef5
Evan Palmer <evanp922@gmail.com>
2023-01-23 19:10:17 -0800

    started implementing terminal configurations and serial device initialization

--- src/alpha_driver/include/alpha_driver/serial_client.hpp
+++ src/alpha_driver/include/alpha_driver/serial_client.hpp
@@ -20,18 +20,76 @@
 
 #pragma once
 
-#include "rclcpp/rclcpp.hpp"
+#include <functional>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "alpha_driver/packet.hpp"
+#include "alpha_driver/packet_id.hpp"
 
 namespace alpha_driver
 {
 
-class SerialClient : public rclcpp::Node
+class SerialClient
 {
 public:
-  SerialClient();
+  /**
+   * @brief Represents the current state of the serial client. Can be either running or stopped.
+   *
+   */
+  enum class Status
+  {
+    kRunning,
+    kStopped,
+  };
+
+  /**
+   * @brief Construct a new Serial Client object
+   *
+   * @param device serial port to connect to
+   * @param baudrate baudrate of the serial connection
+   */
+  SerialClient(const std::string & device, const int baudrate);
+
+  /**
+   * @brief Destroy the Serial Client object. This shuts down all threads.
+   *
+   */
+  ~SerialClient();
+
+  /**
+   * @brief Send a packet over the serial connection.
+   *
+   * @param packet message to send to the Reach Alpha manipulator
+   */
+  void Send(const Packet & packet);
+
+  /**
+   * @brief Register a new callback function to a specified packet type.
+   *
+   * @param packet_type type of packet that the callback should be signaled on
+   * @param callback function that should be executed when a message of a given type is received
+   */
+  void Receive(const PacketId packet_type, std::function<void(Packet)> callback);
 
 private:
-  /* data */
+  // Map used to store the callback functions for messages. Keys should be the ID for a message,
+  // and the values are the callback functions to execute when a message is received.
+  std::unordered_map<PacketId, std::vector<std::function<void(Packet)>>> callbacks_;
+
+  // Serial port pointer
+  int fd_;
+
+  // Flag indicating whether the serial client is running
+  Status running_;
+
+  // Thread responsible for receiving incoming data and executing the respective callbacks
+  std::thread rx_worker_;
+
+  // Incoming data buffer
+  std::vector<unsigned char> buffer_;
 };
 
 }  // namespace alpha_driver
--- src/alpha_driver/src/serial_client.cpp
+++ src/alpha_driver/src/serial_client.cpp
@@ -20,23 +20,46 @@
 
 #include "alpha_driver/serial_client.hpp"
 
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <stdexcept>
+
 #include "alpha_driver/packet.hpp"
 
 namespace alpha_driver
 {
 
-SerialClient::SerialClient()
-: Node("SerialClient")
+SerialClient::SerialClient(const std::string & device, const int baudrate)
 {
+  if (device.empty()) {
+    throw std::runtime_error("Attempted to open file for unassigned file path.");
+  }
+
+  // Open the serial port
+  fd_ = open(device.c_str(), O_RDWR);
+
+  if (fd_ == -1) {
+    throw std::runtime_error(
+      "Could not open specified serial device. Please verify that the device name is correct and "
+      "that you have read/write permissions");
+  }
+
+  // Configure the linux file for serial communication
+  struct termios tty;
+
+  // Get the existing settings
+  if (tcgetattr(fd_, &tty) < 0) {
+    throw std::runtime_error("Unable to get the current terminal configurations.");
+  }
+
+  // TODO: Configure termios settings
+
+  running_ = Status::kRunning;
+
+  // TODO: Start a new thread to receive data
 }
 
 }  // namespace alpha_driver
-
-auto main(int argc, char ** argv) -> int
-{
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<alpha_driver::SerialClient>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
