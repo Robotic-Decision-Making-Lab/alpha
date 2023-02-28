2d4f4615d470f98eafde2781eaee3210c592beaa
Evan Palmer <evanp922@gmail.com>
2023-02-28 11:12:01 -0800

    cleaned up folder structure

--- alpha_bringup/launch/planning.launch.py
+++ alpha_bringup/launch/planning.launch.py
@@ -73,18 +73,12 @@ def generate_launch_description() -> LaunchDescription:
             default_value="true",
             description="Automatically start RViz2",
         ),
-        DeclareLaunchArgument(
-            "use_sim",
-            default_value="false",
-            description="Automatically start Gazebo",
-        ),
     ]
 
     # Initialize Arguments
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
     use_rviz = LaunchConfiguration("use_rviz")
-    use_sim = LaunchConfiguration("use_sim")
     prefix = LaunchConfiguration("prefix")
     namespace = LaunchConfiguration("namespace")
 
@@ -113,7 +107,7 @@ def generate_launch_description() -> LaunchDescription:
                 PathJoinSubstitution(
                     [
                         FindPackageShare(description_package),
-                        "srdf",
+                        "config",
                         "alpha.config.srdf.xacro",
                     ]
                 ),
@@ -207,7 +201,6 @@ def generate_launch_description() -> LaunchDescription:
             moveit_controllers,
             ompl_planning_config,
             planning_scene_monitor_parameters,
-            {"use_sim_time": use_sim},
         ],
     )
 
